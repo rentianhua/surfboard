0318ab2ad91f04eb87d7d85a5de513c78c6b13be
Tim <>
2015-09-21 17:13:33 +0800

    ApiControllers

---
+++ Source/Sites/CCN.WebAPI/ApiControllers/BaseController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CCN.Modules.Base.BusinessEntity;
+using CCN.Modules.Base.Interface;
+using Cedar.Core.IoC;
+
+namespace CCN.WebAPI.ApiControllers
+{
+    public class BaseController : ApiController
+    {
+        private readonly IBaseManagementService _baseservice;
+
+        public BaseController()
+        {
+            _baseservice = ServiceLocatorFactory.GetServiceLocator().GetService<IBaseManagementService>();
+        }
+
+        [Route("/api/Base/GetProvList/{initial}")]
+        [HttpGet]
+        public IEnumerable<BaseProvince> GetProvList(string initial)
+        {
+            return _baseservice.GetProvList(initial);
+        }
+    }
+}
