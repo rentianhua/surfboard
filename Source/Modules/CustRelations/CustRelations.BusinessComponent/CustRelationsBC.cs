604c0c6a54d7c378a4caf5c17b1045acec3056e9
阿布天 <renbuqi@163.com>
2015-10-04 14:40:29 +0800

    提交代码CustomerRelations初始化代码
    提交代码CustomerRelations初始化代码

---
+++ Source/Modules/CustRelations/CustRelations.BusinessComponent/CustRelationsBC.cs
@@ -0,0 +1,23 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using CCN.Modules.CustRelations.BusinessEntity;
+using CCN.Modules.CustRelations.DataAccess;
+using Cedar.Core.IoC;
+using Cedar.Framework.Common.BaseClasses;
+using Cedar.Framework.Common.Server.BaseClasses;
+
+#endregion
+
+namespace CCN.Modules.CustRelations.BusinessComponent
+{
+    /// <summary>
+    /// </summary>
+    public class CustRelationsBC : BusinessComponentBase<CustRelationsDA>
+    {
+        public CustRelationsBC(CustRelationsDA daObject) : base(daObject)
+        {
+        }
+    }
+}
