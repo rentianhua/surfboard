0ba81a4f333dcc136f24feda80305cab946765c2
阿布天 <renbuqi@163.com>
2015-10-10 17:57:08 +0800

    添加twin给微信中间件

---
+++ Source/Sites/CCN.Midware.Wechat/ApiControllers/WeChatCallbackController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+using System.Web;
+using System.Web.Http;
+using Senparc.Weixin.MP;
+using Senparc.Weixin.MP.Entities;
+using Senparc.Weixin.MP.MvcExtension;
+
+namespace CCN.Midware.Wechat.Controllers
+{
+    public class WeChatCallbackController : ApiController
+    {
+        public WeChatCallbackController()
+        {
+        }
+
+        [HttpGet]
+        public WeixinResult DataDispatcher(string signature, string timestamp, string nonce, string echostr)
+        {
+            return !CheckSignature.Check(signature, timestamp, nonce, null) ? new WeixinResult("") : new WeixinResult(echostr);
+        }
+
+        /// <summary>
+        /// 最简化的处理流程（不加密）
+        /// </summary>
+        [HttpPost]
+        public string DataDispatcher([FromBody] string data)
+        {
+            Console.WriteLine(data);
+            return "";
+        }
+    }
+
+
+}
---
+++ Source/Sites/CCN.Midware.Wechat/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+namespace CCN.Midware.Wechat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Microsoft.Owin.Hosting.WebApp.Start<Startup>("http://localhost:8088"))
+            {
+                Console.WriteLine("Press [enter] to quit...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
