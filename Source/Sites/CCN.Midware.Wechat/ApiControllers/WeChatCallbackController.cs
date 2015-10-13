9b6c007a575b1a3a4b59c134c86f621dd16615c2
阿布天 <renbuqi@163.com>
2015-10-13 17:19:45 +0800

    修改微信回调站点
    1.修改微信回调站点
    2.修改cedar的一些引用问题
    3.添加redis的hash实现

--- Source/Sites/CCN.Midware.Wechat/ApiControllers/WeChatCallbackController.cs
+++ Source/Sites/CCN.Midware.Wechat/ApiControllers/WeChatCallbackController.cs
@@ -1,39 +1,65 @@
 ﻿using System;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using System.Net;
-using System.IO;
-using System.Web;
+using System.Configuration;
+using System.Net.Http;
 using System.Web.Http;
 using Senparc.Weixin.MP;
-using Senparc.Weixin.MP.Entities;
-using Senparc.Weixin.MP.MvcExtension;
+using Senparc.Weixin.MP.CommonAPIs;
 
 namespace CCN.Midware.Wechat.Controllers
 {
+    [RoutePrefix("api/WeChatCallback")]
     public class WeChatCallbackController : ApiController
     {
+        private readonly HttpResponseMessage _response;
+        private readonly string _appid = ConfigurationManager.AppSettings["APPID"];
+        private readonly string _appSecret = ConfigurationManager.AppSettings["AppSecret"];
+
         public WeChatCallbackController()
         {
+            _response = new HttpResponseMessage { Content = new StringContent("") };
+            var appid = ConfigurationManager.AppSettings["APPID"];
+            if (!AccessTokenContainer.CheckRegistered(appid))
+                AccessTokenContainer.Register(appid, ConfigurationManager.AppSettings["AppSecret"]);
         }
 
         [HttpGet]
-        public WeixinResult DataDispatcher(string signature, string timestamp, string nonce, string echostr)
+        public HttpResponseMessage DataDispatcher(string signature, string timestamp, string nonce, string echostr)
         {
-            return !CheckSignature.Check(signature, timestamp, nonce, null) ? new WeixinResult("") : new WeixinResult(echostr);
+            Console.WriteLine($"----------------get Start {DateTime.Now}----------------");
+            Console.WriteLine($"----------------{signature}_{timestamp}_{nonce}_{echostr}----------------");
+            Console.WriteLine($"----------------pass:{signature}----------------");
+            Console.WriteLine($"----------------{CheckSignature.Check(signature, timestamp, nonce, "weixin")}----------------");
+            _response.Content = CheckSignature.Check(signature, timestamp, nonce, !string.IsNullOrEmpty(ConfigurationManager.AppSettings["wechattoken"]) ?
+                ConfigurationManager.AppSettings["wechattoken"] : null)
+                ? new StringContent(echostr) : _response.Content;
+            return _response;
         }
 
         /// <summary>
         /// 最简化的处理流程（不加密）
         /// </summary>
         [HttpPost]
-        public string DataDispatcher([FromBody] string data)
+        public HttpResponseMessage DataDispatcher()
         {
-            Console.WriteLine(data);
-            return "";
+            var stream = Request.Content.ReadAsStringAsync().Result;
+            Console.WriteLine($"----------------Start {DateTime.Now}----------------");
+            Console.WriteLine(stream);
+            Console.WriteLine($"----------------End {DateTime.Now}----------------");
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("GetTicket/{appid}")]
+        public string GetTicket(string appid)
+        {
+            return AccessTokenContainer.TryGetAccessToken(_appid, _appSecret);
+        }
+
+        [HttpGet]
+        [Route("GetToken/{appid}")]
+        public string GetToken(string appid)
+        {
+            return AccessTokenContainer.TryGetJsApiTicket(_appid, _appSecret);
         }
     }
-
-
 }
